11164e019c15b23c0909a6c074699f9ff7c836e8
IkramKhanNiazi <ikramfyp@gmail.com>
2024-09-21 14:22:50 +0500

    fist commit

---
+++ dags/final_project.py
@@ -0,0 +1,65 @@
+from datetime import datetime, timedelta
+import pendulum
+import os
+from airflow.decorators import dag
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.postgres_operator import PostgresOperator
+from operators import (StageToRedshiftOperator, LoadFactOperator,
+                       LoadDimensionOperator, DataQualityOperator)
+from helpers import SqlQueries
+
+
+
+default_args = {
+    'owner': 'udacity',
+    'start_date': pendulum.now(),
+}
+
+@dag(
+    default_args=default_args,
+    description='Load and transform data in Redshift with Airflow',
+    schedule_interval='0 * * * *'
+)
+def final_project():
+
+    start_operator = DummyOperator(task_id='Begin_execution')
+
+    stage_events_to_redshift = StageToRedshiftOperator(
+        task_id='Stage_events',
+        table="staging_events",
+        redshift_conn_id="redshift",
+        aws_credentials_id="aws_credentials",
+        s3_bucket="udacity-dend",
+        s3_key="log_data"
+       
+    )
+
+    stage_songs_to_redshift = StageToRedshiftOperator(
+        task_id='Stage_songs',
+    )
+
+    load_songplays_table = LoadFactOperator(
+        task_id='Load_songplays_fact_table',
+    )
+
+    load_user_dimension_table = LoadDimensionOperator(
+        task_id='Load_user_dim_table',
+    )
+
+    load_song_dimension_table = LoadDimensionOperator(
+        task_id='Load_song_dim_table',
+    )
+
+    load_artist_dimension_table = LoadDimensionOperator(
+        task_id='Load_artist_dim_table',
+    )
+
+    load_time_dimension_table = LoadDimensionOperator(
+        task_id='Load_time_dim_table',
+    )
+
+    run_quality_checks = DataQualityOperator(
+        task_id='Run_data_quality_checks',
+    )
+
+final_project_dag = final_project()
